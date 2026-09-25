0567267d55813913be1d131dac180450a52e906a
Sirach1 <147946615+Sirach1@users.noreply.github.com>
2026-09-26 03:59:06 +1200

    Update compiled Python files for views and settings

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User                                                                                                             
 from django.contrib.auth import authenticate, login, logout
-from rest_framework.decorators import api_view, permission_classes, csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
